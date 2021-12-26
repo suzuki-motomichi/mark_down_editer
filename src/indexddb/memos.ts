b6143622fc7a88a9f8b92f05d554ea60c90724dd
suzuki-motomichi <cw.sm31@gmail.com>
2021-12-26 10:35:32 +0900

    add: テーブルへの保存処理

--- src/indexddb/memos.ts
+++ src/indexddb/memos.ts
@@ -12,3 +12,9 @@ const database = new Dexie('markdown-editor')
 database.version(1).stores({ memos: '&datetime' })
 // データを扱うテーブルクラスを取得
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
+
+export const putMemo = async (title: string, text: string): Promise<void> => {
+  // ISOというフォーマットで出力(ISO 8601 は国際規格の日付と時刻の表記)
+  const datetime = new Date().toISOString()
+  await memos.put({ datetime, title, text})
+}

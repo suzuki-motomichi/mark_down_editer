4bf0a843c7d6ce73ac820a99a555b5b7bc31be59
suzuki-motomichi <cw.sm31@gmail.com>
2021-12-30 13:07:48 +0900

    add: htmlをサニタイズ

--- src/worker/convert_markdown_worker.ts
+++ src/worker/convert_markdown_worker.ts
@@ -1,12 +1,15 @@
 // ライブラリのインポート
 import * as marked from 'marked'
+// sanitize-html をインポート
+import * as sanitizeHtml from 'sanitize-html'
 
 // 変数作成、これはTSの書き方でjsならselfにアクセス出来るがtsは型の兼ね合いがある為anyにしてチェックを回避する
 const worker: Worker = self as any;
 
 worker.addEventListener("message", (event) => {
-const text = event.data
-const html = marked(text)
-// メインスレッドからテキストデータをmarkedでHTMLに変換
-worker.postMessage({ html })
+  const text = event.data
+  // ...はスプレッド構文で、元のオブジェクトを変更せず、新しい要素を追加する時(今回はh1とh2)などで使える
+  const html = sanitizeHtml(marked(text), { allowedTags: [...sanitizeHtml.defaults.allowedTags, 'h1', 'h2']})
+  // メインスレッドからテキストデータをmarkedでHTMLに変換
+  worker.postMessage({ html })
 })
